ff2a00c4bab28cd7bbedabf21bd4ddfca75d3a80
SamuMontoya <samu7montoya@gmail.com>
2024-10-04 09:54:55 -0500

    feat: Add invoice data and service

---
+++ src/app/data/invoice.data.ts
@@ -0,0 +1,40 @@
+import { Invoice } from '../models';
+
+export const invoiceData: Invoice = {
+  id: 1,
+  name: 'Computer Components',
+  client: {
+    name: 'John Doe',
+    lastName: 'Smith',
+    address: {
+      country: 'United States',
+      city: 'New York',
+      street: '5th Avenue',
+      number: 15,
+    },
+  },
+  company: {
+    name: 'Tech Store',
+    fiscalNumber: 123456789,
+  },
+  items: [
+    {
+      id: 1,
+      product: 'Motherboard',
+      quantity: 1,
+      price: 250,
+    },
+    {
+      id: 2,
+      product: 'CPU',
+      quantity: 1,
+      price: 350,
+    },
+    {
+      id: 3,
+      product: 'RAM Memory',
+      quantity: 2,
+      price: 100,
+    },
+  ],
+};
---
+++ src/app/models/index.ts
@@ -0,0 +1,5 @@
+export * from './address';
+export * from './client';
+export * from './company';
+export * from './invoice';
+export * from './item';
