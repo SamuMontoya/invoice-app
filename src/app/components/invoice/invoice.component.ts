b3be76163d4ae208f99f77e9f033795a82e17f14
SamuMontoya <samu7montoya@gmail.com>
2024-10-04 11:56:48 -0500

    feat: add layout of invoice

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services';
 import { Invoice } from '../../models';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        beige: "#e6e0be",
+        black: "#131315",
+        orange: "#d7745a",
+        yellow: "#e8c755",
+        gray: "#dacfbc",
+      },
+    },
+  },
+  plugins: [],
+};
