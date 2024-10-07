47f17f20f344a4ddc36b447d0fe8d55d1ca72244
SamuMontoya <samu7montoya@gmail.com>
2024-10-07 11:58:14 -0500

    feat: add empty state

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,16 +2,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Column } from '../../models';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { EmptyDataComponent } from '../empty-data/empty-data.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, EmptyDataComponent],
   templateUrl: './table.component.html',
 })
 export class TableComponent {
   @Input() columns!: Column[];
   @Input() data!: any[];
+  @Input() total!: number;
 
   @Output() removeEmitter: EventEmitter<number> = new EventEmitter<number>();
   onRemove(id: number) {
