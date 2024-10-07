41339817122bb982613b625925a91812cf71563f
SamuMontoya <samu7montoya@gmail.com>
2024-10-07 17:38:24 -0500

    feat: add form validations

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
 })
 export class ButtonComponent {
   @Input() text: string = 'Button';
+  @Input() disabled: boolean = false;
   @Output() onClick: EventEmitter<void> = new EventEmitter();
 
   handleClick(): void {
